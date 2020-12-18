895ac4b1dd09a4f1961e23a534200b243afb5fca
James <info@thecodediver.com>
2020-12-18 12:43:31 -0500

    add test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,15 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(true)
+const server = require("./server.js")
+const request = require("supertest")
+
+
+describe("GET /", () => {
+  it("has process.env.DB_ENV as testing", () => {
+    expect(process.env.NODE_ENV).toBe("testing")
+  })
+
+  it("returns 200 OK", () => {
+    return request(server).get("/")
+    .expect(200)
+  })
 })
